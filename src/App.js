106a8a975586725e2546071006a74a0f7e1e60c2
Debarghya Datta <ujan.dattaa@gmail.com>
2022-08-06 00:01:58 +0530

    context

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import Footer from './components/layout/Footer'
 import Home from './pages/Home'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
+import { GithubProvider } from './context/github/GithubContext'
 
 
 function App() {
   return (
-    <Router>
-      <div className='flex flex-col justify-between h-screen'>
-        <Navbar/>
-        <main className='container mx-auto px-3 pb-12'>
-        <Routes>
-                <Route path='/'
+    <GithubProvider>
+        <Router>
+          <div className='flex flex-col justify-between h-screen'>
+            <Navbar />
+
+            <main className='container mx-auto px-3 pb-12'>
+              <Routes>
+                <Route
+                  path='/'
                   element={
                     <>
                       <Home />
@@ -23,12 +27,15 @@ function App() {
                 <Route path='/about' element={<About />} />
                 <Route path='/notfound' element={<NotFound />} />
                 <Route path='*' element={<NotFound />} />
-        </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  );
+              </Routes>
+            </main>
+
+            <Footer />
+          </div>
+        </Router>
+      {/* </AlertProvider> */}
+    </GithubProvider>
+  )
 }
 
-export default App;
+export default App
